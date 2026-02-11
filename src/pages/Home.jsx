07a1354e72d31dc3ffb54017d7e6421f7598580e
mohamedzidan2006 <a.z01150171478@gmail.com>
2026-02-11 22:31:45 +0200

    Section2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import Section1 from "../components/Section1";
-
+import Section2 from "../components/Section2";
 export default function Home() {
   return (
     <div className="w-full ">
       <Section1 />
+      <Section2/>
     </div>
   );
 }
