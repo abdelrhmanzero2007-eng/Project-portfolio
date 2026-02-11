04e248fccedf63a3ff0e520ba4a4cab7000351be
abdelrahma <abdelrhman.zero.2007@gmail.com>
2026-02-11 14:25:25 +0200

    finsh navbar !

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,44 +1,73 @@
+import { useState } from "react"; // 1. استيراد useState
 import { BsCart2 } from "react-icons/bs";
 import { FaRegCircleUser } from "react-icons/fa6";
-import { HiBars3 } from "react-icons/hi2";
+import { HiMiniBars3 } from "react-icons/hi2";
 import { IoMdHeartEmpty } from "react-icons/io";
 import { IoSearchOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
-
-// <div className="w-full flex items-center justify-between bg-amber-600 py-4 px-8 lg:py-7 lg:px-41.25"></div>
+import logo from "../assets/logo.png";
 
 export default function NavBar() {
+  // 2. حالة القائمة (مفتوحة أو مقفولة)
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="w-full flex items-center justify-between bg-amber-600 py-4 px-8 lg:py-7 lg:px-41.25">
-      {" "}
-      <div className="block lg:hidden">
-        <HiBars3 />
+    <nav className="relative w-full bg-white border-b ">
+      <div className="flex items-center text-Main justify-between py-4 px-8 lg:py-7 lg:px-41.25">
+        <div
+          className="block lg:hidden cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <HiMiniBars3 className="h-6 w-6" />
+        </div>
+
+        <div className="">
+          <img src={logo} alt="Logo" />
+        </div>
+
+        <div className="hidden lg:block">
+          <ul className="flex items-center justify-evenly gap-12 font-medium">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex items-center justify-evenly gap-6 text-xl">
+          <IoSearchOutline className="hidden lg:block cursor-pointer" />
+          <FaRegCircleUser className="hidden lg:block cursor-pointer" />
+          <IoMdHeartEmpty className="hidden lg:block cursor-pointer" />
+          <BsCart2 className="cursor-pointer" />
+        </div>
       </div>
-      <div className="">
-        <h1>Logo</h1>
-      </div>
-      <div className="flex-none hidden lg:block">
-        <ul className="flex items-center justify-evenly gap-15">
-          <li>
+
+      <div
+        className={`${isOpen ? "block" : "hidden"} lg:hidden bg-white text-Main border-t absolute w-full left-0 z-50 shadow-lg`}
+      >
+        <ul className="flex flex-col p-4 gap-4 font-medium pl-8">
+          <li onClick={() => setIsOpen(false)}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li onClick={() => setIsOpen(false)}>
             <Link to="/shop">Shop</Link>
           </li>
-          <li>
-            <Link to="/cart">About</Link>
+          <li onClick={() => setIsOpen(false)}>
+            <Link to="/about">About</Link>
           </li>
-          <li>
-            <Link to="/login">Contact</Link>
+          <li onClick={() => setIsOpen(false)}>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </div>
-      <div className="flex items-center justify-evenly gap-6">
-        <IoSearchOutline className="hidden lg:block" />
-        <FaRegCircleUser className="hidden lg:block" />
-        <IoMdHeartEmpty className="hidden lg:block" />
-        <BsCart2 />
-      </div>
-    </div>
+    </nav>
   );
 }
