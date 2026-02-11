fa9bd0856eaead212932334e3c39d1e38e421035
abdelrahma <abdelrhman.zero.2007@gmail.com>
2026-02-11 21:10:29 +0200

    first jop teem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,24 @@
+import img from "../assets/image1.png";
+import img2 from "../assets/image.png";
+
 export default function Home() {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
+    <div
+      style={{ backgroundImage: `url(${img})` }}
+      className="w-full h-[640px] px-41.25 bg-no-repeat bg-cover bg-center "
+    >
+      <div className="w-full md:w-105 h-full bg-[#826f66] items-center justify-center flex flex-col text-center gap-10.5">
+        <img src={img2} alt="Image" />
+        <p className="text-white text-[16px] font-sans leading-6 ">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, odio!
+        </p>
+        <h1 className="text-4xl text-MainWhite font-classic text-[36px] font-bold leading-11 tracking-[4%] ">
+          Welcome to Our Website
+        </h1>
+        <button className="px-5 py-3.75 bg-MainWhite text-[#826f66] font-bold font-sans ">
+          {" "}
+          SHOP NOW{" "}
+        </button>
       </div>
     </div>
   );
